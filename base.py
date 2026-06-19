5b076d6a27cd91d38af89fc1553354c5ebc72037
mija <est.alvarez.diego@latecnicalf.com.ar>
2026-06-19 09:06:01 -0300

    db

--- base.py
+++ base.py
@@ -13,27 +13,48 @@ CREATE TABLE IF NOT EXISTS usuarios (
 
 cur.execute("""
 CREATE TABLE IF NOT EXISTS categoria (
-    legajo_categoria INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_categoria INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre_categoria VARCHAR(35) NOT NULL UNIQUE   
 )
 """)
 
 cur.execute("""
 CREATE TABLE IF NOT EXISTS marca (
-    legajo_marca INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_marca INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre_marca VARCHAR(35) NOT NULL UNIQUE
 )
 """)
 
 cur.execute("""
 CREATE TABLE IF NOT EXISTS productos (
-    legajo_ptoducto INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_producto INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre VARCHAR(35) NOT NULL UNIQUE,
     stock INTEGER NOT NULL,
     precio INTEGER NOT NULL,
     legajo_marca INTEGER,
     legajo_categoria INTEGER,
-    FOREIGN KEY (legajo_marca) REFERENCES marca(legajo_marca)
-    FOREIGN KEY (legajo_categoria) REFERENCES marca(legajo_categoria)
+    FOREIGN KEY (legajo_marca) REFERENCES marca(legajo_marca),
+    FOREIGN KEY (legajo_categoria) REFERENCES categoria(legajo_categoria)
 )
 """)
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS proveedores (
+    id_proveedor INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre VARCHAR(35) NOT NULL UNIQUE,
+    id_producto INTEGER,
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+)
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS ventas (
+    num_venta INTEGER PRIMARY KEY AUTOINCREMENT,
+    cant_vendida INTEGER NOT NULL,
+    id_producto INTEGER,
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+)
+""")
+
+conn.commit()
+conn.close()
